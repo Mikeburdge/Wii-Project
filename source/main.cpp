88dab7b2dc24727661850af31bb73aaa05d26357
Michael <michael.burdge@hotmail.co.uk>
2021-04-27 15:11:44 +0100

    formatting again

--- source/main.cpp
+++ source/main.cpp
@@ -20,10 +20,9 @@ class SceneID;
 
 using namespace std;
 
-
-//TODO: LOOK INTO WHETHER I SHOULD HAVE u8 OR float OR u64 for limits 
-u8 LOW_LIMIT = 0.0167f;          // Keep At/Below 60fps
-u8 HIGH_LIMIT = 0.1f;            // Keep At/Above 10fps
+//TODO: LOOK INTO WHETHER I SHOULD HAVE u8 OR float OR u64 for limits
+u8 LOW_LIMIT = 0.0167f; // Keep At/Below 60fps
+u8 HIGH_LIMIT = 0.1f;   // Keep At/Above 10fps
 
 /**
  * Program entry point.
@@ -36,64 +35,58 @@ u8 HIGH_LIMIT = 0.1f;            // Keep At/Above 10fps
 int main(int argc, char **argv)
 {
 
-Debug::Log("start of main");
+  Debug::Log("start of main");
 
-//Initializes the VIDEO subsystem
-VIDEO_Init();
+  //Initializes the VIDEO subsystem
+  VIDEO_Init();
 
-float lastTime = gettime();
+  float lastTime = gettime();
 
-//Calls Initial Functions (apparently, cant find anything what it is)
-__lwp_watchdog_init();
+  //Calls Initial Functions (apparently, cant find anything what it is)
+  __lwp_watchdog_init();
 
-//Singletons:
-//Entity
-EntitySystem* SysEntity = EntitySystem::GetInstance();
-//Graphics
-GraphicsSystem* SysGraphics = GraphicsSystem::GetInstance();
-//Physics
-PhysicsSystem* SysPhysics = PhysicsSystem::GetInstance();
-//AI
-AISystem* SysAI = AISystem::GetInstance();
+  //Singletons:
+  //Entity
+  EntitySystem *SysEntity = EntitySystem::GetInstance();
+  //Graphics
+  GraphicsSystem *SysGraphics = GraphicsSystem::GetInstance();
+  //Physics
+  PhysicsSystem *SysPhysics = PhysicsSystem::GetInstance();
+  //AI
+  AISystem *SysAI = AISystem::GetInstance();
 
-//init the graphics system
-SysGraphics->InitGXVideo();
+  //init the graphics system
+  SysGraphics->InitGXVideo();
 
-//Load the first scene we want to see
-SysEntity->LoadScene(SceneID::SceneName::Testing);
+  //Load the first scene we want to see
+  SysEntity->LoadScene(SceneID::SceneName::Testing);
 
   while (true)
   {
-      //Keeps track of the current time
-      float currentTime = gettime();
-      //gets delta time by removing the previous time from current time and dividing by 1000
-      float deltaTime = ( currentTime - lastTime ) / 1000.0f;
+    //Keeps track of the current time
+    float currentTime = gettime();
+    //gets delta time by removing the previous time from current time and dividing by 1000
+    float deltaTime = (currentTime - lastTime) / 1000.0f;
 
-      //Limits the deltatime to its min and max
-      if ( deltaTime < LOW_LIMIT )
-        deltaTime = LOW_LIMIT;
-      else if ( deltaTime > HIGH_LIMIT )
-        deltaTime = HIGH_LIMIT;
+    //Limits the deltatime to its min and max
+    if (deltaTime < LOW_LIMIT)
+      deltaTime = LOW_LIMIT;
+    else if (deltaTime > HIGH_LIMIT)
+      deltaTime = HIGH_LIMIT;
 
-      lastTime = currentTime;
+    lastTime = currentTime;
 
-      //Update the different systems passign through deltatime
-      SysGraphics->Update(deltaTime);
-      SysPhysics->Update(deltaTime);
-      SysAI->Update(deltaTime);
-      SysEntity->Update(deltaTime);
-      
-      
-    
+    //Update the different systems passign through deltatime
+    SysGraphics->Update(deltaTime);
+    SysPhysics->Update(deltaTime);
+    SysAI->Update(deltaTime);
+    SysEntity->Update(deltaTime);
+  }
 
-    
+  delete SysGraphics;
+  delete SysPhysics;
+  delete SysAI;
+  delete SysEntity;
 
-    }
-
-    delete SysGraphics;
-    delete SysPhysics;
-    delete SysAI;
-    delete SysEntity;
-    
-    return 0;
+  return 0;
 }
