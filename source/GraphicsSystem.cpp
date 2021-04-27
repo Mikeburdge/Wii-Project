e1f8cfbc8a562e80a524c5d3adc65b197e6d276b
Michael <michael.burdge@hotmail.co.uk>
2021-04-27 14:56:26 +0100

    formatting

--- source/GraphicsSystem.cpp
+++ source/GraphicsSystem.cpp
@@ -4,8 +4,9 @@
 
 using namespace std;
 
-GraphicsSystem::GraphicsSystem(){
-	
+GraphicsSystem::GraphicsSystem()
+{
+
 	///Init vars
 	camera = {0.0F, 0.0F, 0.0F};
 	up = {0.0F, 1.0F, 0.0F};
@@ -22,20 +23,19 @@ GraphicsSystem::GraphicsSystem(){
 	InitGXVideo();
 }
 
-GraphicsSystem* GraphicsSystem::myInstance = 0;
+GraphicsSystem *GraphicsSystem::myInstance = 0;
 
-GraphicsSystem * GraphicsSystem::GetInstance()
+GraphicsSystem *GraphicsSystem::GetInstance()
 {
 	if (!myInstance)
 	{
 		myInstance = new GraphicsSystem();
 	}
-    return myInstance;
+	return myInstance;
 }
 
 void GraphicsSystem::Init()
-{    
-
+{
 }
 
 void GraphicsSystem::Update(float deltaTime)
@@ -43,31 +43,32 @@ void GraphicsSystem::Update(float deltaTime)
 
 	// create a viewing matrix
 	guLookAt(view, &camera, &up, &look);
-	
-	GX_Begin(GX_TRIANGLES,GX_VTXFMT0,3);
 
+	GX_Begin(GX_TRIANGLES, GX_VTXFMT0, 3);
 }
 
+void GraphicsSystem::InitGXVideo()
+{
 
-void GraphicsSystem::InitGXVideo(){
-	
 	VIDEO_Init();
-	
+
 	videoMode = VIDEO_GetPreferredMode(NULL);
 
-	for(uint8_t videoIndex = 0; videoIndex < FRAMEBUFFER_SIZE; videoIndex++) {
+	for (uint8_t videoIndex = 0; videoIndex < FRAMEBUFFER_SIZE; videoIndex++)
+	{
 		videoFrameBuffer[videoIndex] = (uint32_t *)MEM_K0_TO_K1(SYS_AllocateFramebuffer(videoMode));
 		VIDEO_ClearFrameBuffer(videoMode, videoFrameBuffer[videoIndex], COLOR_BLACK);
 	}
 	videoFrameBufferIndex = 0;
-	
+
 	VIDEO_Configure(videoMode);
 	VIDEO_SetNextFramebuffer(videoFrameBuffer[videoFrameBufferIndex]);
 	VIDEO_SetBlack(FALSE);
 	VIDEO_Flush();
 	VIDEO_WaitVSync();
-	
-	if(videoMode->viTVMode & VI_NON_INTERLACE) {
+
+	if (videoMode->viTVMode & VI_NON_INTERLACE)
+	{
 		VIDEO_WaitVSync();
 	}
 
@@ -83,7 +84,7 @@ void GraphicsSystem::InitGXVideo(){
 	f32 yscale = GX_GetYScaleFactor(videoMode->efbHeight, videoMode->xfbHeight);
 	uint32_t xfbHeight = GX_SetDispCopyYScale(yscale);
 
-	GX_SetViewport(0, 0,videoMode->fbWidth,videoMode->efbHeight, 0, 1);
+	GX_SetViewport(0, 0, videoMode->fbWidth, videoMode->efbHeight, 0, 1);
 	GX_SetScissor(0, 0, videoMode->fbWidth, videoMode->efbHeight);
 	GX_SetDispCopySrc(0, 0, videoMode->fbWidth, videoMode->efbHeight);
 	GX_SetDispCopyDst(videoMode->fbWidth, xfbHeight);
@@ -92,21 +93,23 @@ void GraphicsSystem::InitGXVideo(){
 	// Store graphics system width and height
 	gsWidth = (uint32_t)videoMode->fbWidth;
 	gsHeight = (uint32_t)videoMode->efbHeight;
-	
+
 	GX_SetFieldMode(videoMode->field_rendering, ((videoMode->viHeight == 2 * videoMode->xfbHeight) ? GX_ENABLE : GX_DISABLE));
 
 	// Anti-aliasing initialization
-	if(videoMode->aa){
+	if (videoMode->aa)
+	{
 		GX_SetPixelFmt(GX_PF_RGB565_Z16, GX_ZC_LINEAR);
 	}
-	else{
+	else
+	{
 		GX_SetPixelFmt(GX_PF_RGB8_Z24, GX_ZC_LINEAR);
 	}
 
 	GX_SetCullMode(GX_CULL_NONE);
 	GX_CopyDisp(videoFrameBuffer[videoFrameBufferIndex], GX_TRUE);
 	GX_SetDispCopyGamma(GX_GM_1_0);
-	
+
 	// set number of rasterized color channels
 	GX_SetNumChans(1);
 
@@ -116,48 +119,51 @@ void GraphicsSystem::InitGXVideo(){
 	//Invalidates the vertex cache and current texture memory cache
 	GX_InvVtxCache();
 	GX_InvalidateTexAll();
-	
 
 	// setup our projection matrix
 	// this creates a perspective matrix with a view angle of 90,
 	// and aspect ratio based on the display resolution
-    f32 w = videoMode->viWidth;
-    f32 h = videoMode->viHeight;	
+	f32 w = videoMode->viWidth;
+	f32 h = videoMode->viHeight;
 	//Render mtx
-	guPerspective(projection, 45, (f32)w/h, 0.1F, 1000.0F);
+	guPerspective(projection, 45, (f32)w / h, 0.1F, 1000.0F);
 	GX_LoadProjectionMtx(projection, GX_PERSPECTIVE);
 }
 
-void DrawHLine (int x1, int x2, int y, int color) {
+void DrawHLine(int x1, int x2, int y, int color)
+{
 	int i;
 	y = 320 * y;
 	x1 >>= 1;
 	x2 >>= 1;
-	for (i = x1; i <= x2; i++) {
-		
-		for(uint8_t videoIndex = 0; videoIndex < FRAMEBUFFER_SIZE; videoIndex++) {
-		u32 *tmpfb = (uint32_t *)MEM_K0_TO_K1(SYS_AllocateFramebuffer(videoMode));
-		tmpfb[y+i] = color;
-	}
+	for (i = x1; i <= x2; i++)
+	{
 
-
+		for (uint8_t videoIndex = 0; videoIndex < FRAMEBUFFER_SIZE; videoIndex++)
+		{
+			u32 *tmpfb = (uint32_t *)MEM_K0_TO_K1(SYS_AllocateFramebuffer(videoMode));
+			tmpfb[y + i] = color;
+		}
 	}
 }
 
-void DrawVLine (int x, int y1, int y2, int color) {
+void DrawVLine(int x, int y1, int y2, int color)
+{
 	int i;
 	x >>= 1;
-	for (i = y1; i <= y2; i++) {
+	for (i = y1; i <= y2; i++)
+	{
 		u32 *tmpfb = xfb;
 		tmpfb[x + (640 * i) / 2] = color;
 	}
 }
 
-void DrawBox (int x1, int y1, int x2, int y2, int color) {
-	DrawHLine (x1, x2, y1, color);
-	DrawHLine (x1, x2, y2, color);
-	DrawVLine (x1, y1, y2, color);
-	DrawVLine (x2, y1, y2, color);
+void DrawBox(int x1, int y1, int x2, int y2, int color)
+{
+	DrawHLine(x1, x2, y1, color);
+	DrawHLine(x1, x2, y2, color);
+	DrawVLine(x1, y1, y2, color);
+	DrawVLine(x2, y1, y2, color);
 }
 
-GraphicsSystem::~GraphicsSystem(){}
+GraphicsSystem::~GraphicsSystem() {}
